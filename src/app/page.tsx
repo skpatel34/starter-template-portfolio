3665c974fb82753e0d4e21ec621324712d049d63
smit <smitkanani45@gmail.com>
2024-10-01 16:15:47 +0530

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,28 @@
+
+import { AboutSection } from '@/sections/About'
+import { ContactSection } from '@/sections/Contact'
+import { Footer } from '@/sections/Footer'
+import { Header } from '@/sections/Header'
+import Hero from '@/sections/Hero'
+import { ProjectsSection } from '@/sections/Projects'
+import { TapeSection } from '@/sections/Tape'
+import { TestimonialsSection } from '@/sections/Testimonials'
+
+
+
+const page = () => {
+  return (
+    <div>
+      <Header />
+      <Hero />
+      <ProjectsSection />
+      <TapeSection />
+      <TestimonialsSection />
+      <AboutSection />
+      <ContactSection />
+      <Footer />
+    </div>
+  )
+}
+
+export default page
